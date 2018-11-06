e9c67c1661f722029b927e1bb16b37a36abb1a88
Alexander Christensen <alexander.bondgaard@live.dk>
2018-11-06 14:39:38 +0100

    Potted Plant

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -6,7 +6,35 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+    class PottedPlant
+    {
+        public int numberOfLeaves;
+        public int[] plantColor;
+        public bool isReal;
+        public bool isInPot;
+        public string isPlantWatered;
+
+
+        public void Lookatplant()
+        {
+
+        }
+        public void Photosynthesis()
+        {
+
+        }
+        public void Removeleaves()
+        {
+
+        }
+        public void Waterplant()
+        {
+
+        }
+
+
     }
 }
